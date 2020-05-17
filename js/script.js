ff362b6b6fe2f672daecec2420a45c75d70c703c
zTranquilizer <61971694+zTranquilizer@users.noreply.github.com>
2020-05-17 14:22:40 +0300

    add menu burger
    add menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(".header__burger").click(function (event) {
-    $(".header__burger,.header__menu").toggleClass("active");
+    $(".header__burger,.header__menu,.header").toggleClass("active");
     $("body").toggleClass("lock");
   });
 });
